524cd2c1d3a3b00e6a1aa376d34b55b3f35c4955
Kyu Seong Kim <ksjmgrkks@naver.com>
2023-01-26 10:17:09 +0900

    Update Solution.kt

--- LeetCode/Arrays101/Find All Numbers Disappeared in an Array/Solution.kt
+++ LeetCode/Arrays101/Find All Numbers Disappeared in an Array/Solution.kt
@@ -1,4 +1,16 @@
 class Solution {
+    // 내 풀이
+    fun findDisappearedNumbers(nums: IntArray): List<Int> {
+        val max = nums.size
+        val arr = arrayListOf<Int>()
+        for (i in 1..max) {
+            if (!nums.contains(i))
+                arr.add(i)
+        }
+        return arr
+    }
+    
+    // 정답
     fun findDisappearedNumbers(nums: IntArray): List<Int> {
         for (i in nums.indices) {
             val newIndex = Math.abs(nums[i]) - 1
